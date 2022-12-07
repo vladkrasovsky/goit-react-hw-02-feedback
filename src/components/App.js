9fb5fd73bbc6ada1cbab03d50808e92c5f5ecd54
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-07 22:23:04 +0200

    Setups basic layout

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
+
+export const App = () => {
+  return (
+    <Layout>
+      <GlobalStyle />
+    </Layout>
+  );
+};
